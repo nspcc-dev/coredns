7030e81e0a285b301e8252f5d1082b1bb9ae8b7e
Denis Kirillov <denis@nspcc.ru>
2021-09-23 12:12:31 +0300

    [#6] Correct response record type
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- plugin/nns/nns.go
+++ plugin/nns/nns.go
@@ -3,6 +3,9 @@ package nns
 import (
 	"context"
 	"fmt"
+	"net"
+	"strings"
+
 	"github.com/coredns/coredns/plugin"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/coredns/coredns/request"
@@ -10,7 +13,6 @@ import (
 	nns "github.com/nspcc-dev/neo-go/examples/nft-nd-nns"
 	"github.com/nspcc-dev/neo-go/pkg/core/state"
 	"github.com/nspcc-dev/neo-go/pkg/rpc/client"
-	"strings"
 )
 
 type NNS struct {
@@ -20,11 +22,10 @@ type NNS struct {
 	Log    clog.P
 }
 
-// ServeDNS implements the plugin.Handler interface. This method gets called when example is used
-// in a Server.
+// ServeDNS implements the plugin.Handler interface.
+// This method gets called when example is used in a Server.
 func (n NNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	state := request.Request{W: w, Req: r}
-	s, err := n.resolve(state.QName(), state.QType())
+	res, err := n.resolveRecord(request.Request{W: w, Req: r})
 	if err != nil {
 		n.Log.Warning(err)
 		return plugin.NextOrFailure(n.Name(), n.Next, ctx, w, r)
@@ -32,8 +33,7 @@ func (n NNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (in
 
 	m := new(dns.Msg)
 	m.SetReply(r)
-	hdr := dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeTXT, Class: state.QClass(), Ttl: 0}
-	m.Answer = []dns.RR{&dns.TXT{Hdr: hdr, Txt: []string{s}}}
+	m.Answer = []dns.RR{res}
 
 	w.WriteMsg(m)
 	return dns.RcodeSuccess, nil
@@ -42,21 +42,52 @@ func (n NNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (in
 // Name implements the Handler interface.
 func (n NNS) Name() string { return pluginName }
 
-func (n NNS) resolve(name string, dnsType uint16) (string, error) {
-	var nnsType nns.RecordType
-	switch dnsType {
-	case dns.TypeTXT:
-		nnsType = nns.TXT
-	case dns.TypeA:
-		nnsType = nns.A
-	case dns.TypeAAAA:
-		nnsType = nns.AAAA
-	case dns.TypeCNAME:
-		nnsType = nns.CNAME
-	default:
-		return "", fmt.Errorf("usupported record type: %s", dns.Type(dnsType))
-	}
-
+func (n NNS) resolve(name string, nnsType nns.RecordType) (string, error) {
 	name = strings.TrimSuffix(name, ".")
 	return n.Client.NNSResolve(n.CS.Hash, name, nnsType)
+}
+
+func (n NNS) resolveRecord(state request.Request) (dns.RR, error) {
+	nnsType, err := getNNSType(state)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := n.resolve(state.QName(), nnsType)
+	if err != nil {
+		return nil, err
+	}
+
+	return formResRecord(state, s)
+}
+
+func getNNSType(req request.Request) (nns.RecordType, error) {
+	switch req.QType() {
+	case dns.TypeTXT:
+		return nns.TXT, nil
+	case dns.TypeA:
+		return nns.A, nil
+	case dns.TypeAAAA:
+		return nns.AAAA, nil
+	case dns.TypeCNAME:
+		return nns.CNAME, nil
+	}
+	return 0, fmt.Errorf("usupported record type: %s", dns.Type(req.QType()))
+}
+
+func formResRecord(req request.Request, res string) (dns.RR, error) {
+	hdr := dns.RR_Header{Name: req.QName(), Rrtype: req.QType(), Class: req.QClass(), Ttl: 0}
+
+	switch req.QType() {
+	case dns.TypeTXT:
+		return &dns.TXT{Hdr: hdr, Txt: []string{res}}, nil
+	case dns.TypeA:
+		return &dns.A{Hdr: hdr, A: net.ParseIP(res)}, nil
+	case dns.TypeAAAA:
+		return &dns.AAAA{Hdr: hdr, AAAA: net.ParseIP(res)}, nil
+	case dns.TypeCNAME:
+		return &dns.CNAME{Hdr: hdr, Target: res}, nil
+	}
+
+	return nil, fmt.Errorf("usupported record type: %s", dns.Type(req.QType()))
 }
