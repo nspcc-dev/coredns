06f0c0a3a520d212d39e88404b0f631b67a7115b
Angira Kekteeva <kira@nspcc.ru>
2021-12-14 17:21:22 +0300

    [#19] Add tests
    Signed-off-by: Angira Kekteeva <kira@nspcc.ru>

---
+++ plugin/healthchecker/healthchecker_test.go
@@ -0,0 +1,39 @@
+package healthchecker
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/miekg/dns"
+	"github.com/stretchr/testify/require"
+)
+
+type tmpcheck struct {
+}
+
+func (t *tmpcheck) Check(record dns.RR) bool {
+	return true
+}
+
+func TestPanic(t *testing.T) {
+	checker := &tmpcheck{}
+
+	f, err := NewHealthCheckFilter(checker, 2, 200, map[string]struct{}{})
+
+	require.NoError(t, err)
+
+	a := &dns.A{A: net.ParseIP("127.0.0.1")}
+	a2 := &dns.A{A: net.ParseIP("127.0.0.2")}
+	a3 := &dns.A{A: net.ParseIP("127.0.0.3")}
+	a4 := &dns.A{A: net.ParseIP("127.0.0.4")}
+
+	f.put(a)
+	f.put(a2)
+	time.Sleep(200 * time.Millisecond)
+	f.put(a3)
+	time.Sleep(200 * time.Millisecond)
+	f.put(a4)
+
+	time.Sleep(time.Second)
+}
