9ea07dbd83e8a7caf15d3699c2b72aae3ea5c862
Denis Kirillov <denis@nspcc.ru>
2022-10-31 11:55:22 +0300

    [#32] nns: Support multiple contracts
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- plugin/nns/nns.go
+++ plugin/nns/nns.go
@@ -4,26 +4,30 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/nns/contract"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/coredns/coredns/plugin/transfer"
 	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 	nns "github.com/nspcc-dev/neo-go/examples/nft-nd-nns"
-	"github.com/nspcc-dev/neo-go/pkg/rpc/client"
-	"github.com/nspcc-dev/neo-go/pkg/util"
 )
 
 type NNS struct {
-	Next         plugin.Handler
-	Client       *client.Client
-	ContractHash util.Uint160
-	Log          clog.P
-	nnsDomain    string
-	dnsDomain    string
+	Next      plugin.Handler
+	Contracts []*contract.Contract
+	Log       clog.P
+	dnsDomain string
+}
+
+type Records struct {
+	Name string
+	Type nns.RecordType
+	Data []string
 }
 
 const dot = "."
@@ -49,9 +53,10 @@ func (n NNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (in
 func (n NNS) Name() string { return pluginName }
 
 // Transfer implements the transfer.Transfer interface.
+// Don't use serial because we support only AXFR.
 func (n NNS) Transfer(zone string, serial uint32) (<-chan []dns.RR, error) {
-	trimmedZone := n.prepareName(zone)
-	records, err := getRecords(n.Client, n.ContractHash, trimmedZone, nns.RecordType(dns.TypeSOA))
+	trimmedZone := n.Contracts[0].PrepareName(zone, n.dnsDomain)
+	records, err := n.Contracts[0].GetRecords(trimmedZone, nns.RecordType(dns.TypeSOA))
 	if err != nil {
 		n.Log.Warningf("couldn't transfer zone '%s' as '%s': %s", zone, trimmedZone, err.Error())
 		return nil, transfer.ErrNotAuthoritative
@@ -64,9 +69,9 @@ func (n NNS) Transfer(zone string, serial uint32) (<-chan []dns.RR, error) {
 	go func() {
 		defer close(ch)
 
-		recs, err := n.zoneTransfer(trimmedZone)
+		recs, err := n.zoneTransfers(zone)
 		if err != nil {
-			n.Log.Warningf("couldn't transfer zone '%s' as '%s' : %s", zone, trimmedZone, err.Error())
+			n.Log.Warningf("couldn't transfer zone '%s': %s", zone, err.Error())
 			return
 		}
 
@@ -80,101 +85,158 @@ func (n *NNS) setDNSDomain(name string) {
 	n.dnsDomain = strings.Trim(name, dot)
 }
 
-func (n *NNS) setNNSDomain(name string) {
-	n.nnsDomain = strings.Trim(name, dot)
-}
-
-func (n NNS) prepareName(name string) string {
-	name = strings.TrimSuffix(name, dot)
-	if n.nnsDomain != "" {
-		name = strings.TrimSuffix(strings.TrimSuffix(name, n.dnsDomain), dot)
-		if name != "" {
-			name += dot
-		}
-		name += n.nnsDomain
-	}
-	return name
-}
-
 func (n NNS) resolveRecords(state request.Request) ([]dns.RR, error) {
-	name := n.prepareName(state.QName())
+	var err error
+	var result []dns.RR
+
+	for _, nnsContract := range n.Contracts {
+		result, err = n.resolveContractRecords(nnsContract, state)
+		if err != nil {
+			n.Log.Warningf("resolve in contract '%s': %s", nnsContract.Hash().StringLE(), err.Error())
+		}
+	}
+
+	return result, nil
+}
+
+func (n NNS) resolveContractRecords(nnsContract *contract.Contract, state request.Request) ([]dns.RR, error) {
+	name := nnsContract.PrepareName(state.QName(), n.dnsDomain)
 
 	nnsType, err := getNNSType(state)
 	if err != nil {
-		return nil, fmt.Errorf("cannot resolve '%s' (type %d) as '%s': %w", state.QName(), state.QType(), name, err)
+		return nil, fmt.Errorf("cannot resolve '%s' (type %d) as '%s': %w",
+			state.QName(), state.QType(), name, err)
 	}
 
-	resolved, err := resolve(n.Client, n.ContractHash, name, nnsType)
+	resolved, err := nnsContract.Resolve(name, nnsType)
 	if err != nil {
-		return nil, fmt.Errorf("cannot resolve '%s' (type %d) as '%s': %w", state.QName(), state.QType(), name, err)
+		return nil, fmt.Errorf("cannot resolve '%s' (type %d) as '%s': %w",
+			state.QName(), state.QType(), name, err)
 	}
 
 	hdr := dns.RR_Header{Name: state.QName(), Rrtype: state.QType(), Class: state.QClass(), Ttl: 0}
 	res, err := formResRecords(hdr, resolved)
 	if err != nil {
-		return nil, fmt.Errorf("cannot resolve '%s' (type %d) as '%s': %w", state.QName(), state.QType(), name, err)
+		return nil, fmt.Errorf("cannot resolve '%s' (type %d) as '%s': %w",
+			state.QName(), state.QType(), name, err)
 	}
+
 	return res, nil
 }
 
-func (n NNS) zoneTransfer(name string) ([]dns.RR, error) {
-	records, err := getAllRecords(n.Client, n.ContractHash, name)
-	if err != nil {
-		return nil, err
-	}
+func (n NNS) zoneTransfers(zone string) ([]dns.RR, error) {
+	var result map[string]*Records
+	for i, nnsContract := range n.Contracts {
+		transferRecords, err := n.allTransferRecords(nnsContract, zone, i == 0)
+		if err != nil {
+			n.Log.Warningf("get all records in contract '%s': %s", nnsContract.Hash().StringLE(), err.Error())
+		}
 
-	numSoa, index := 0, -1
-	for i, record := range records {
-		records[i].Name = appendRoot(record.Name)
-		if record.Type == nns.RecordType(dns.TypeSOA) {
-			numSoa++
-			index = i
+		for key, recs := range transferRecords {
+			result[key] = recs
 		}
 	}
-	if numSoa != 1 {
-		return nil, fmt.Errorf("invalid number of soa records: %d", numSoa)
-	}
 
-	if index != 0 {
-		records[0], records[index] = records[index], records[0]
-	}
-	return formZoneTransfer(records)
+	return formZoneTransfer(result)
 }
 
-func formZoneTransfer(records []nnsRecord) ([]dns.RR, error) {
-	if len(records) == 0 {
-		return nil, fmt.Errorf("records must not be empty")
-	}
-	soaRecord, err := formSoaRecord(records[0])
+func (n NNS) allTransferRecords(nnsContract *contract.Contract, zone string, needSOA bool) (map[string]*Records, error) {
+	name := nnsContract.PrepareName(zone, n.dnsDomain)
+
+	records, err := nnsContract.GetAllRecords(name)
 	if err != nil {
 		return nil, err
 	}
 
-	results := make([]dns.RR, 1, len(records)+1)
-	results[0] = soaRecord
-	for _, record := range records[1:] {
-		rec, err := formRec(uint16(record.Type), record.Data, dns.RR_Header{
-			Name:   record.Name,
-			Rrtype: uint16(record.Type),
-			Class:  dns.ClassINET,
-		})
-		if err != nil {
-			return nil, err
+	result := make(map[string]*Records)
+	for _, record := range records {
+		updatedName := appendRoot(record.Name)
+		recs := result[recKey(updatedName, record.Type)]
+		if recs == nil {
+			recs = &Records{
+				Name: updatedName,
+				Type: record.Type,
+				Data: []string{},
+			}
 		}
-		results = append(results, rec)
+		recs.Data = append(recs.Data, record.Data)
 	}
+
+	if needSOA {
+		soaRecs := result[recKey(appendRoot(name), nns.RecordType(dns.TypeSOA))]
+		if len(soaRecs.Data) != 1 {
+			return nil, fmt.Errorf("invalid number of soa records: %d", len(soaRecs.Data))
+		}
+	}
+
+	return result, nil
+}
+
+func recKey(name string, recType nns.RecordType) string {
+	return name + strconv.Itoa(int(recType))
+}
+
+func formZoneTransfer(recordsMap map[string]*Records) ([]dns.RR, error) {
+	if len(recordsMap) == 0 {
+		return nil, fmt.Errorf("records must not be empty")
+	}
+
+	records := make([]*Records, 0, len(recordsMap))
+	for _, rec := range recordsMap {
+		records = append(records, rec)
+	}
+
+	sort.Slice(records, func(i, j int) bool {
+		if records[i].Name == records[j].Name {
+			return records[i].Type < records[j].Type
+		}
+		return records[i].Name < records[j].Name
+	})
+
+	var err error
+	var soaRecord *dns.SOA
+	results := make([]dns.RR, 1, len(records))
+
+	for _, recs := range records {
+		if recs.Type == nns.RecordType(dns.TypeSOA) {
+			soaRecord, err = formSoaRecord(recs)
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		for _, data := range recs.Data {
+			rec, err := formRec(uint16(recs.Type), data, dns.RR_Header{
+				Name:   recs.Name,
+				Rrtype: uint16(recs.Type),
+				Class:  dns.ClassINET,
+			})
+			if err != nil {
+				return nil, err
+			}
+			results = append(results, rec)
+		}
+	}
+
+	results[0] = soaRecord
 	results = append(results, soaRecord)
 
 	return results, nil
 }
 
-func formSoaRecord(rec nnsRecord) (*dns.SOA, error) {
+func formSoaRecord(rec *Records) (*dns.SOA, error) {
 	if rec.Type != nns.RecordType(dns.TypeSOA) {
 		return nil, fmt.Errorf("invalid type for soa record")
 	}
-	split := strings.Split(rec.Data, " ")
+
+	if len(rec.Data) != 1 {
+		return nil, fmt.Errorf("invalid len for soa record")
+	}
+
+	split := strings.Split(rec.Data[0], " ")
 	if len(split) != 7 {
-		return nil, fmt.Errorf("invalid soa record: %s", rec.Data)
+		return nil, fmt.Errorf("invalid soa record: %s", rec.Data[0])
 	}
 
 	name := appendRoot(split[0])
